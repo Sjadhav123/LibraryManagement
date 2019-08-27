885bfa9b24bc68ee89f255ed2b6500002f50e5a2
SMalage <SMalage@SILICUS217.silicus.com>
2019-08-27 17:01:07 +0530

    service layer created for entities

--- LibraryManagement/src/main/java/com/silicus/librarymanagent/Service/BookIssueTracker/BookIssueTrackerServiceImpl.java
+++ LibraryManagement/src/main/java/com/silicus/librarymanagent/Service/BookIssueTracker/BookIssueTrackerServiceImpl.java
@@ -5,34 +5,41 @@ import java.util.List;
 
 public class BookIssueTrackerServiceImpl<T> implements BookIssueTrackerService<T> {
 
+	@Override
 	public T read(Serializable primaryKey) throws IllegalStateException, IllegalArgumentException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
 	public T pudate(T t) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
 	public T insert(T t) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
 	public List<T> findAll() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
+	@Override
 	public boolean delete(Long id) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	@Override
 	public long count() {
 		// TODO Auto-generated method stub
 		return 0;
 	}
+
 
 }
